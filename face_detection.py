16551df1a2192304f7b03901ac48f11460ce6c52
n-jabes <nshutij7@gmail.com>
2023-05-11 08:13:34 +0200

    finished programm

---
+++ face_detection.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+
+def detect_and_save_faces(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    cap = cv2.VideoCapture(0)
+
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    while True:
+        ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            face = frame[y:y+h, x:x+w]
+
+            filename = os.path.join(folder, f'face_{len(os.listdir(folder))}.jpg')
+
+            cv2.imwrite(filename, face)
+
+        cv2.imshow('Face Detection', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+save_folder = 'detected_faces'
+
+detect_and_save_faces(save_folder)
